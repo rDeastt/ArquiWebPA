96cd53097e837b7d3aeffd8c5126fd79fdd2589d
green <14atcamposo@gmail.com>
2023-10-25 12:58:39 -0500

    Revert "tEST"
    This reverts commit c96450d8285f7f5c1b2af5a3ca53a176492a3bfc.

--- src/main/java/com/upc/arquiwebpa/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.upc.arquiwebpa.repository;
-
-import com.upc.arquiwebpa.entities.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
